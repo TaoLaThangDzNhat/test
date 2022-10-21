8b69cae9d8a80437ce1a3e4aeeb32561e41c6da1
TaoLaThangDzNhat <116358966+TaoLaThangDzNhat@users.noreply.github.com>
2022-10-21 20:00:17 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Chào nhóc nhé ! 🖐🖐🖐',
+    introDesc: `Ku trả lời đi`,
+    btnIntro: 'OK',
+    title: 'Anh đẹp trai lắm phải không ? 😙',
+    desc: 'Ku mà thoát ra là anh đẹp trai đó:> ',
+    btnYes: 'Có anh đẹp trai 💖💖💖',
+    btnNo: '💔 Anh mơ hả ! 💔 ',
+    question:'Nói cho anh biết sao anh đẹp trai đi 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Vì anh là gu của em😊😊',
+    mess: 'Anh biết mà, Anh cũng thích em lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Anh nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/100030281270127'
+}
